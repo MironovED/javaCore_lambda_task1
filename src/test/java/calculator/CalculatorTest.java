16fc870c7124c1743a5670d879b1a45a3dfa97a2
Mironov Evgeny <e.d.mironov@yandex.ru>
2026-02-18 18:37:14 +0300

    добавлены юнит тесты

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,97 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("Тест на проверку сложения")
+    public void testPlus() {
+        //given:
+        int x = 5;
+        int y = 7;
+        //when:
+        Calculator calc = Calculator.instance.get();
+        int result = calc.plus.apply(x, y);
+        //then:
+        assertEquals(12, result);
+    }
+
+    @Test
+    @DisplayName("Тест на проверку вычитания")
+    public void testMinus() {
+        //given:
+        int x = 10;
+        int y = 3;
+        //when:
+        Calculator calc = Calculator.instance.get();
+        int result = calc.minus.apply(x, y);
+        //then:
+        assertEquals(7, result);
+    }
+
+    @Test
+    @DisplayName("Тест на проверку умножения")
+    public void testMultiply() {
+        //given:
+        int x = 5;
+        int y = 7;
+        //when:
+        Calculator calc = Calculator.instance.get();
+        int result = calc.multiply.apply(x, y);
+        //then:
+        assertEquals(35, result);
+    }
+
+    @Test
+    @DisplayName("Тест на проверку деления")
+    public void testDevide() {
+        //given:
+        int x = 21;
+        int y = 7;
+        //when:
+        Calculator calc = Calculator.instance.get();
+        int result = calc.devide.apply(x, y);
+        //then:
+        assertEquals(3, result);
+    }
+
+    @Test
+    @DisplayName("Тест на проверку деления на ноль")
+    public void testDevideOnZero() {
+        //given:
+        int x = 21;
+        int y = 0;
+        //when:
+        Calculator calc = Calculator.instance.get();
+        //then:
+        assertThrows(ArithmeticException.class, () -> calc.devide.apply(x, y));
+    }
+
+    @Test
+    @DisplayName("Тест на проверку инкримента")
+    public void testPow() {
+        //given:
+        int x = 5;
+        //when:
+        Calculator calc = Calculator.instance.get();
+        int result = calc.pow.apply(x);
+        //then:
+        assertEquals(25, result);
+    }
+
+    @Test
+    @DisplayName("Тест на проверку положительного числа")
+    public void testIsPositive() {
+        //given:
+        int x = 1;
+        //when:
+        Calculator calc = Calculator.instance.get();
+        boolean result = calc.isPositive.test(x);
+        //then:
+        assertTrue(result);
+    }
+}
